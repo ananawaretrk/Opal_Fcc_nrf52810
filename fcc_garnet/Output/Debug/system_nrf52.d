12219556623cf4ab9c7caaa7fae7f92ddc588eb3
purvesh <58706866+ptrackonomy@users.noreply.github.com>
2023-03-09 10:55:46 -0800

    Major Update
    Replicated fcc_jade to create fcc_garnet

---
+++ fcc_garnet/Output/Debug/system_nrf52.d
@@ -0,0 +1,17 @@
+Output/Debug//system_nrf52.o: \
+ C:\Users\Purvesh\Documents\GitHub\TrackFW_FCC\3rd_party\nRF5_SDK_16\modules\nrfx\mdk\system_nrf52.c \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.40/include/stdint.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.40/include/stdbool.h \
+ C:\Users\Purvesh\Documents\GitHub\TrackFW_FCC\3rd_party\nRF5_SDK_16\modules\nrfx\mdk\nrf.h \
+ C:\Users\Purvesh\Documents\GitHub\TrackFW_FCC\3rd_party\nRF5_SDK_16\modules\nrfx\mdk\nrf52.h \
+ ../3rd_party/nRF5_SDK_16/components/toolchain/cmsis/include/core_cm4.h \
+ ../3rd_party/nRF5_SDK_16/components/toolchain/cmsis/include/core_cmInstr.h \
+ ../3rd_party/nRF5_SDK_16/components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../3rd_party/nRF5_SDK_16/components/toolchain/cmsis/include/core_cmFunc.h \
+ ../3rd_party/nRF5_SDK_16/components/toolchain/cmsis/include/core_cmSimd.h \
+ C:\Users\Purvesh\Documents\GitHub\TrackFW_FCC\3rd_party\nRF5_SDK_16\modules\nrfx\mdk\system_nrf52.h \
+ C:\Users\Purvesh\Documents\GitHub\TrackFW_FCC\3rd_party\nRF5_SDK_16\modules\nrfx\mdk\system_nrf.h \
+ C:\Users\Purvesh\Documents\GitHub\TrackFW_FCC\3rd_party\nRF5_SDK_16\modules\nrfx\mdk\nrf52_bitfields.h \
+ C:\Users\Purvesh\Documents\GitHub\TrackFW_FCC\3rd_party\nRF5_SDK_16\modules\nrfx\mdk\nrf51_to_nrf52.h \
+ C:\Users\Purvesh\Documents\GitHub\TrackFW_FCC\3rd_party\nRF5_SDK_16\modules\nrfx\mdk\nrf52_name_change.h \
+ C:\Users\Purvesh\Documents\GitHub\TrackFW_FCC\3rd_party\nRF5_SDK_16\modules\nrfx\mdk\compiler_abstraction.h
