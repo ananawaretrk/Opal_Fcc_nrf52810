9884d6d27cfaf2b076f17472414e071b4ef63b30
purvesh <58706866+ptrackonomy@users.noreply.github.com>
2023-08-28 10:47:41 -0700

    Opal introduced
    Derivation of fcc_onyx_2.

---
+++ fcc_opal_1/Output/Debug/nrf_ringbuf.d
@@ -0,0 +1,45 @@
+Output/Debug//nrf_ringbuf.o: \
+ C:\Users\Purvesh\Documents\GitHub\TrackFW_FCC\3rd_party\nRF5_SDK_16\components\libraries\ringbuf\nrf_ringbuf.c \
+ C:\Users\Purvesh\Documents\GitHub\TrackFW_FCC\3rd_party\nRF5_SDK_16\components\libraries\ringbuf\nrf_ringbuf.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.40/include/stdint.h \
+ ../3rd_party/nRF5_SDK_16/components/libraries/atomic/nrf_atomic.h \
+ ../3rd_party/nRF5_SDK_16/components/libraries/util/sdk_common.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.40/include/stdbool.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.40/include/string.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.40/include/__crossworks.h \
+ C:/Users/Purvesh/Documents/GitHub/TrackFW_FCC/fcc_onyx_2_sm/sdk_config.h \
+ ../3rd_party/nRF5_SDK_16/components/libraries/util/nordic_common.h \
+ ../3rd_party/nRF5_SDK_16/modules/nrfx/mdk/compiler_abstraction.h \
+ ../3rd_party/nRF5_SDK_16/components/libraries/util/sdk_os.h \
+ ../3rd_party/nRF5_SDK_16/components/libraries/util/sdk_errors.h \
+ ../3rd_party/nRF5_SDK_16/components/softdevice/s132/headers/nrf_error.h \
+ ../3rd_party/nRF5_SDK_16/components/libraries/util/app_util.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.40/include/stddef.h \
+ ../3rd_party/nRF5_SDK_16/modules/nrfx/mdk/nrf.h \
+ ../3rd_party/nRF5_SDK_16/modules/nrfx/mdk/nrf52.h \
+ ../3rd_party/nRF5_SDK_16/components/toolchain/cmsis/include/core_cm4.h \
+ ../3rd_party/nRF5_SDK_16/components/toolchain/cmsis/include/core_cmInstr.h \
+ ../3rd_party/nRF5_SDK_16/components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../3rd_party/nRF5_SDK_16/components/toolchain/cmsis/include/core_cmFunc.h \
+ ../3rd_party/nRF5_SDK_16/components/toolchain/cmsis/include/core_cmSimd.h \
+ ../3rd_party/nRF5_SDK_16/modules/nrfx/mdk/system_nrf52.h \
+ ../3rd_party/nRF5_SDK_16/modules/nrfx/mdk/system_nrf.h \
+ ../3rd_party/nRF5_SDK_16/modules/nrfx/mdk/nrf52_bitfields.h \
+ ../3rd_party/nRF5_SDK_16/modules/nrfx/mdk/nrf51_to_nrf52.h \
+ ../3rd_party/nRF5_SDK_16/modules/nrfx/mdk/nrf52_name_change.h \
+ ../3rd_party/nRF5_SDK_16/modules/nrfx/mdk/compiler_abstraction.h \
+ ../3rd_party/nRF5_SDK_16/components/softdevice/s132/headers/nrf52/nrf_mbr.h \
+ ../3rd_party/nRF5_SDK_16/components/softdevice/s132/headers/nrf_svc.h \
+ ../3rd_party/nRF5_SDK_16/components/libraries/util/sdk_macros.h \
+ ../3rd_party/nRF5_SDK_16/components/libraries/util/nrf_assert.h \
+ ../3rd_party/nRF5_SDK_16/components/libraries/util/sdk_errors.h \
+ ../3rd_party/nRF5_SDK_16/components/libraries/util/app_util_platform.h \
+ ../3rd_party/nRF5_SDK_16/components/softdevice/s132/headers/nrf_soc.h \
+ ../3rd_party/nRF5_SDK_16/components/softdevice/s132/headers/nrf_svc.h \
+ ../3rd_party/nRF5_SDK_16/components/softdevice/s132/headers/nrf_error.h \
+ ../3rd_party/nRF5_SDK_16/components/softdevice/s132/headers/nrf_error_soc.h \
+ ../3rd_party/nRF5_SDK_16/components/softdevice/s132/headers/nrf_nvic.h \
+ ../3rd_party/nRF5_SDK_16/components/libraries/util/app_error.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.40/include/stdio.h \
+ ../3rd_party/nRF5_SDK_16/components/libraries/util/app_error_weak.h \
+ ../3rd_party/nRF5_SDK_16/components/libraries/util/nrf_assert.h
---
+++ fcc_opal_1/Output/Release/thumb_crt0.d
@@ -0,0 +1,2 @@
+Output/Release//thumb_crt0.o: \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.40/source/thumb_crt0.s
