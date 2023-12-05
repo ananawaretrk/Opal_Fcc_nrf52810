bc64f874abcc64fe813b5e41d944be7fc5763b9a
purvesh <58706866+ptrackonomy@users.noreply.github.com>
2023-12-05 14:47:04 -0800

    Opal_1_1_x_Modem_test_esim_V1.0
    Search for ltem using esim test profile

--- fcc_onyx_2_sm/main.cpp
+++ fcc_onyx_2_sm/main.cpp
@@ -55,9 +55,9 @@
 #include "LIS3DH.h"
 
 // ---------------------------------- Board Defines------------------------------------------------------
-#define ONYX_2
+//#define ONYX_2
 //#define RIM_1_2 // Added support to run on RIM V1.2.0 also
-//#define OPAL_1_1_X
+#define OPAL_1_1_X
 
 // ---------------------------------- Sub-Board Defines------------------------------------------------------
 #ifdef RIM_1_2
@@ -2380,13 +2380,13 @@ sm_state modem_network_config()
 {
     nrf_gpio_pin_set(CELL_ENABLE_PIN_O);
     nrf_delay_ms(2000);
-    
+
     init_Modem();
     nrf_delay_ms(1000);
     
-    nbiot_instance.SetConfig(1); // LTE-M or NB-IOT no preference
+    //nbiot_instance.SetConfig(1); // LTE-M or NB-IOT no preference
     //nbiot_instance.SetConfig(2); // NB-IOT Only
-    //nbiot_instance.SetConfig(3); // LTE-M Only
+    nbiot_instance.SetConfig(3); // LTE-M Only
     
     #ifdef ESIM_TEST_PROFILE_ENABLE
     printf("Activating TS.48 test profile on esim\n");
